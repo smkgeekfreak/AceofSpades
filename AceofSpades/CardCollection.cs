aeeba1f9c0c1bd9a1c4747ffb81920251d1ac43f
smkgeekfreak <smkgeekfreak@gmail.com>
2013-10-24 12:04:25 -0500

    moved extension to their own file

--- AceofSpades/CardCollection.cs
+++ AceofSpades/CardCollection.cs
@@ -14,12 +14,20 @@ namespace AceofSpades
         public virtual void Initalize() { _cards = new List<Card>(); }
         public abstract void Insert(Card insertCard);
         public abstract void Delete(Card deleteCard);
+        /*
+         * Promoted Discard method from Card to CardCollection as it makes
+         * more sense for the CardCollection to traverse the association the 
+         * Cards it contains than vice versa.  Another way to do this would
+         * have been to raise an event from Card and then search the collection. 
+         * Additionally the problem stated that a discard would be place into a 
+         * draw pile not a discard pile which seems wrong but is supported.  
+         */
         public void Discard(Card card, CardCollection cardCollection)
         {
             if (Cards.IsNullOrEmpty())
                 throw new ArgumentException("No cards to discard");
 
-            if (Cards.Contains(card)) 
+            if (Cards.Contains(card))
             {
                 Delete(card);
                 cardCollection.Insert(card);
@@ -29,22 +37,4 @@ namespace AceofSpades
         }
     }
 
-    public static class MyExtensions
-    {
-        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
-        {
-            if (enumerable == null)
-                return true;
-
-            return !enumerable.Any();
-        }
-
-        public static IEnumerable<T> FindAllAfter<T>(this IEnumerable<T> enumerable, Func<T,bool> match)
-        {
-            if (enumerable == null)
-                return null;
-
-            return enumerable.Where(match);
-        }
-    }
 }
---
+++ AceofSpades/CardExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AceofSpades
+{
+    public static class CardExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            if (enumerable == null)
+                return true;
+
+            return !enumerable.Any();
+        }
+
+    }
+}
